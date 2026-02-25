0733aa765d5fee534ff0f15c06ddd1142264f654
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2026-02-25 11:34:22 +0100

    chore: upgrade Angular to v21

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import en from '@angular/common/locales/en';
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { routes } from './app.routes';
@@ -10,11 +9,5 @@ import { routes } from './app.routes';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideNzI18n(en_US),
-    provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
-  ],
+  providers: [provideZonelessChangeDetection(), provideRouter(routes), provideNzI18n(en_US), provideHttpClient(withFetch())],
 };
